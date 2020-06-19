700a65c9ec604dabfd043d453ab0165314adadc4
Toby Thain <toby.thain@fivebox.com>
2020-06-18 23:19:32 -0400

    Add pen handling.

--- src/main/scala/tutorial/webapp/VectorData.scala
+++ src/main/scala/tutorial/webapp/VectorData.scala
@@ -9,24 +9,48 @@ import scala.scalajs.js.Dynamic.global
 
 object VectorData {
 
-  sealed trait VCommand {
+  // command:
+  // instruction   parameter field   terminator
+  // parameter field:
+  // - integer -32768..+32767 or -16384..16383
+  // - decimal +/-127.999
+  // - label fields: any combination of text,
+  //   numeric expressions, or string variables
+  // * when using the label instruction LB,
+  //   the terminator used must be a binary 3 (ETX)
+  // * certain instructions such as PA or PR may have
+  //   multiple or optional parameters. These parameters
+  //   must be separated by commas
+
+  sealed trait HpGlPen {
+    def number: Int
+  }
+  case object BeamOff extends HpGlPen { def number = 0 }
+  case object FullBrightness extends HpGlPen { def number = 1 }
+  case object HalfBrightness extends HpGlPen { def number = 2 }
+  case object DimBrightness extends HpGlPen { def number = 3 }
+
+  sealed trait HpGlCommand {
     def n(x: Double): String = Math.round(x).toString
-    def ns(ps: List[(Double,Double)]): String =
+    def ns(ps: Iterable[(Double,Double)]): String =
       ps.map{ case (x,y) => s"${n(x)},${n(y)}" }.mkString(",")
 
     def text: String
   }
-  case object PenUp extends VCommand {
+  case object PenUp extends HpGlCommand {
     def text = "PU"
   }
-  case object PenDown extends VCommand {
+  case object PenDown extends HpGlCommand {
     def text = "PD"
   }
-  case class PlotAbs(pt: (Double,Double), more: List[(Double,Double)] = Nil) extends VCommand {
-    def text = s"PA ${ns(pt :: more)}"
+  case class PlotAbs(ps: (Double,Double)*) extends HpGlCommand {
+    def text = s"PA ${ns(ps)}"
   }
-  case class PlotRel(pt: (Double,Double), more: List[(Double,Double)] = Nil) extends VCommand {
-    def text = s"PR ${ns(pt :: more)}"
+  case class PlotRel(ps: (Double,Double)*) extends HpGlCommand {
+    def text = s"PR ${ns(ps)}"
+  }
+  case class SelectPen(pen: HpGlPen) extends HpGlCommand {
+    def text = s"SP ${pen.number}"
   }
 
   def run(): Unit = {
@@ -38,7 +62,7 @@ object VectorData {
     val cols = Math.ceil(Math.sqrt(gs.length))
 
     gs.zipWithIndex.flatMap { case (g,idx) =>
-      // notionally the graph has nodes labelled 1..order
+      // graph nodes are labelled 1..order
 
       def pos(i: Int): (Double,Double) = {
         val cx = ((idx % cols)+1) * graphSpacing
@@ -48,20 +72,7 @@ object VectorData {
          cy + graphRadius*Math.sin(a))
       }
 
-      // command:
-      // instruction   parameter field   terminator
-      // parameter field:
-      // - integer -32768..+32767 or -16384..16383
-      // - decimal +/-127.999
-      // - label fields: any combination of text,
-      //   numeric expressions, or string variables
-      // * when using the label instruction LB,
-      //   the terminator used must be a binary 3 (ETX)
-      // * certain instructions such as PA or PR may have
-      //   multiple or optional parameters. These parameters
-      //   must be separated by commas
-
-      g.toList.flatMap {
+      val edges = g.toList.flatMap {
         case Edge(u, v) =>
           List(
             PenUp,
@@ -70,6 +81,19 @@ object VectorData {
             PlotAbs(pos(v))
           )
       }
+
+      val nodes = (1 to order).flatMap { node =>
+        val (x,y) = pos(node)
+        List(
+          PenUp,
+          PlotAbs( (x-4,y) ),
+          PenDown,
+          PlotRel( (4,-4), (4,4), (-4,4), (-4,-4) )
+        )
+      }.toList
+
+      SelectPen(FullBrightness) :: (edges ++ (SelectPen(HalfBrightness) :: nodes))
+
     }.foreach(c => println(c.text))
   }
 
