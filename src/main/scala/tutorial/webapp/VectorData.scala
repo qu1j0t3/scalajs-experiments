7eaa37a9eb653f60f6cd936459d677e40e5ee3c0
Toby Thain <toby.thain@fivebox.com>
2020-07-26 22:05:18 -0400

    Update with successful tests with HP 1347A.

--- src/main/scala/tutorial/webapp/VectorData.scala
+++ src/main/scala/tutorial/webapp/VectorData.scala
@@ -1,5 +1,7 @@
 package tutorial.webapp
 
+import java.io.{BufferedWriter, File, FileWriter}
+
 import tutorial.webapp.GraphEnumerate.Edge
 
 object VectorData {
@@ -7,6 +9,14 @@ object VectorData {
   // Parameters for HP 1347A vector display
   val xMax = 2047
   val yMax = 1512
+
+  // GPIB adapter setup to communicate interactively with HP:
+  // ++v 1        ; interactive mode
+  // ++addr 1     ; device address
+  // ++auto 0     ; don't wait for a response after each message
+  // Configure a newline tx delay of 1 milliseconds
+  // (if communication is not clean, increase this to 5ms or more)
+  // This assumes 1 HPGL command per line/message.
 
   def run(): Unit = {
     val order = 4
@@ -17,15 +27,15 @@ object VectorData {
     // Aim for a squarish layout
     val cols = Math.ceil(Math.sqrt(gs.length))
     val rows = (gs.length + cols - 1)/cols
-    val graphSpacing = yMax / (rows+2); // Height of screen is less than width
+    val graphSpacing = yMax / rows; // Height of screen is less than width
     val graphRadius = graphSpacing * 0.3
 
-    gs.zipWithIndex.flatMap { case (g,idx) =>
+    val commands = gs.zipWithIndex.flatMap { case (g,idx) =>
       // graph nodes are labelled 1..order
 
       def pos(i: Int): (Double,Double) = {
         val cx = xCentre + ((idx % cols) - (cols-1)/2.0) * graphSpacing
-        val cy = yCentre + (Math.floor(idx / cols) - (rows-1)/2.0) * graphSpacing
+        val cy = yCentre - (Math.floor(idx / cols) - (rows-1)/2.0) * graphSpacing
         val a = 2*Math.PI*i/order
         (cx + graphRadius*Math.cos(a),
          cy + graphRadius*Math.sin(a))
@@ -41,19 +51,38 @@ object VectorData {
           )
       }
 
+      val connectedNodes = g.toList.flatMap { case Edge(u, v) => List(u, v) }.toSet
+
       val nodes = (1 to order).flatMap { node =>
-        val (x,y) = pos(node)
-        List(
-          PenUp,
-          PlotAbs( (x-4,y) ),
-          PenDown,
-          PlotRel( (4,-4), (4,4), (-4,4), (-4,-4) )
-        )
+        // Don't draw nodes that are part of an edge
+        if (!connectedNodes.contains(node)) {
+          val (x, y) = pos(node)
+          List(
+            PenUp,
+            PlotAbs((x - 4, y - 4)),
+            PenDown,
+            PlotRel((4, 4)),
+            PenUp,
+            PlotRel((-4, 0)),
+            PenDown,
+            PlotRel((4, -4))
+          )
+        } else {
+          Nil
+        }
       }.toList
 
-      SelectPen(FullBrightness) :: (edges ++ (SelectPen(HalfBrightness) :: nodes))
+      //SelectPen(HalfBrightness) :: (edges ++ (SelectPen(FullBrightness) :: nodes))
+      edges ++ nodes
 
-    }.foreach(c => println(c.text))
+    }
+
+    //commands.foreach(cmd => println(cmd.text + ";"))
+
+    val file = new File(s"/Users/toby/Documents/git/scalajs-experiments/hpgl-order$order.txt")
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(commands.map(_.text + ";\n").mkString)
+    bw.close()
   }
 
 }
