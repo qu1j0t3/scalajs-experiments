1d1bc9a501a8c9728688e77867de22c23735397c
Toby Thain <toby.thain@fivebox.com>
2020-08-25 20:51:33 -0400

    Fixed maze generator.

--- src/main/scala/tutorial/webapp/MazeDemo.scala
+++ src/main/scala/tutorial/webapp/MazeDemo.scala
@@ -14,7 +14,6 @@ object MazeDemo {
    *
    * 1. Choose a starting point
    * 2. Crawl forward: randomly choose a wall and delete it if adjacent cell has not been visited
-   *    (this ensures any cell has only one path into it?)
    * 3. if all adjacent cells have been visited, backtrack
    * 4. algorithm ends when we have backtracked to starting point
    *
@@ -51,36 +50,37 @@ object MazeDemo {
 
     val allCells = (for {i <- 0 until w; j <- 0 until h} yield (i,j)).toSet
 
-    @tailrec def step(i: Int, j: Int, walls: Set[(Int, Int, Dir)], unvisited: Set[(Int, Int)]): Set[(Int, Int, Dir)] = {
-      val updatedUnvisited = unvisited - ((i,j))
-
-      val possibleWalls =
-        (if (i > 0   && unvisited.contains((i-1,j)) && walls.contains((i,j,Dir.w)))   List(Dir.w) else Nil) ++
-        (if (i < w-1 && unvisited.contains((i+1,j)) && walls.contains((i+1,j,Dir.w))) List(Dir.e) else Nil) ++
-        (if (j > 0   && unvisited.contains((i,j-1)) && walls.contains((i,j,Dir.n)))   List(Dir.n) else Nil) ++
-        (if (j < h-1 && unvisited.contains((i,j+1)) && walls.contains((i,j+1,Dir.n))) List(Dir.s) else Nil)
-
-      if (possibleWalls.isEmpty) {
-        if (unvisited.isEmpty) {
-          walls
-        } else {
-          // visit a random new cell
-          val (newi,newj) = unvisited.toVector/*yikes*/(Math.abs(Random.nextInt) % unvisited.size)
-          step(newi, newj, walls, updatedUnvisited)
-        }
+    def step(i: Int, j: Int, walls: Set[(Int, Int, Dir)], unvisited: Set[(Int, Int)]): (Set[(Int, Int, Dir)],Set[(Int, Int)]) = {
+      if (unvisited.isEmpty) {
+        (walls,unvisited)
       } else {
-        // remove wall and change position
-        val (newi,newj,newWalls) = possibleWalls(Math.abs(Random.nextInt) % possibleWalls.size) match {
-          case Dir.North => (i,j-1,walls - ((i,j,Dir.n)))
-          case Dir.West  => (i-1,j,walls - ((i,j,Dir.w)))
-          case Dir.South => (i,j+1,walls - ((i,j+1,Dir.n)))
-          case Dir.East  => (i+1,j,walls - ((i+1,j,Dir.w)))
+        val updatedUnvisited = unvisited - ((i, j))
+
+        val possibleWalls =
+          (if (i > 0) List(Dir.w) else Nil) ++
+          (if (i < w - 1) List(Dir.e) else Nil) ++
+          (if (j > 0) List(Dir.n) else Nil) ++
+          (if (j < h - 1) List(Dir.s) else Nil)
+
+        // shuffle available directions
+        possibleWalls.map((Random.nextInt(), _)).sortBy(_._1).map(_._2).foldLeft(
+          (walls, updatedUnvisited)
+        ) { case ((w, u), d) => // remove wall and change position
+          val (newi, newj, newWalls) = d match {
+            case Dir.North => (i, j - 1, w - ((i, j, Dir.n)))
+            case Dir.West => (i - 1, j, w - ((i, j, Dir.w)))
+            case Dir.South => (i, j + 1, w - ((i, j + 1, Dir.n)))
+            case Dir.East => (i + 1, j, w - ((i + 1, j, Dir.w)))
+          }
+          if (u.contains( (newi,newj) ))
+            step(newi, newj, newWalls, u)
+          else
+            (w,u)
         }
-        step(newi, newj, newWalls, updatedUnvisited)
       }
     }
 
-    step(0, 0, walls, allCells)
+    step(0, 0, walls, allCells)._1
   }
 
   def printMaze(w: Int, h: Int, walls: Set[(Int, Int, Dir)]): Unit = {
