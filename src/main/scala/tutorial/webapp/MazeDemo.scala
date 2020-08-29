cbf9fa9b102942ca4a9878d8c445624ca63318f4
Toby Thain <toby.thain@fivebox.com>
2020-08-28 23:18:32 -0400

    Link joined edges.

--- src/main/scala/tutorial/webapp/MazeDemo.scala
+++ src/main/scala/tutorial/webapp/MazeDemo.scala
@@ -87,9 +87,12 @@ object MazeDemo {
      A strategy to do this:
      Consider the lines to be drawn as edges in the graph that can be directly defined from the wall set.
      Until there are no degree 1 nodes left in the graph:
-       For each degree 1 node, follow the single edge from that node
-       and continue following edges in the same direction until there are no more.
-       Draw the line segment formed by these edges and eliminate these edges from the graph.
+       For each degree 1 node, start a new path at the node, follow the single edge from that node
+       and continue following edges in the same direction until reaching a node with no edges
+       continuing in that direction.
+       Add the line segment formed by the followed edges and eliminate these edges from the graph.
+       If there is a single edge from that node, begin following it in that direction.
+       Otherwise this path is finished.
 
      The remaining edges are part of closed paths.
 
@@ -97,16 +100,11 @@ object MazeDemo {
        Pick an arbitrary node, then follow one edge from it, in the same direction as far as we can,
        collecting the edges into a single new segment.
        Then start a new direction and do the same, until the starting node is reached.
-
    */
 
   def graph(walls: Set[(Int, Int, Dir)]) = {
     // Nodes are tuples (i,j)
-    // Edges are defined by a predicate adjacent (a,b) where adjacent (a,b) also implies adjacent (b,a) (undirected)
-    // collect all edges defined by walls
-    // maintain a function degree(n) for all nodes
-    // when we add an edge (a,b), a != b, we increase the degree of both a and b
-    // alternatively, maintain a neighbours set for all nodes
+    // maintain a neighbours set for all nodes
     walls.foldLeft(
       //Set[(Int,Int,Int,Int)](), // edges
       Map[(Int,Int),Set[(Int,Int)]]() // node -> neighbours
@@ -126,7 +124,56 @@ object MazeDemo {
             nbrs + ((i,j) -> (newNbrs + ((i,j+1)))) + ((i,j+1) -> (southNbrs + ((i,j))))
         }
     }
+  }
 
+  def linkEdges(nbrs: Map[(Int,Int),Set[(Int,Int)]]) = {
+    def sgn(v: Int) = if (v < 0) -1 else if (v > 0) 1 else 0
+
+    // Result is a single path, plus a set of new nodes to follow
+    def follow(
+                head: (Int,Int),
+                prev: Option[(Int,Int)],
+                dx: Int, dy: Int,
+                branches: Set[(Int,Int)]
+              ): (List[(Int,Int)],Set[(Int,Int)],Option[(Int,Int)]) = {
+      val ns = nbrs(head)
+      val forward = ns -- prev.toSet // ignore the node we're coming from
+      val nextSameDir = (head._1+dx,head._2+dy)
+      if (forward.contains(nextSameDir)) {
+        // can continue in same direction
+        // FIXME: When we do this, we need to eliminate this neighbour (head) from the neighbour sets
+        //        for each path that was NOT chosen.
+        //        This will create disconnected (cut off) paths that need to be followed later!
+        follow(nextSameDir, Some(head), dx, dy, if(forward.size > 1) branches + head else branches)
+      } else if (forward.size == 1) {
+        // change direction
+        val nextNewDir = forward.head
+        val (path,bs,endpoint) = follow(nextNewDir, Some(head), sgn(nextNewDir._1 - head._1), sgn(nextNewDir._2 - head._2), branches)
+        (head :: path, bs, endpoint)
+      } else if (forward.isEmpty) {
+        // cannot continue - reached isolated endpoint of path
+        (List(head), branches, Some(head))
+      } else {
+        // cannot continue - reached a T-junction
+        (List(head), branches, None)
+      }
+    }
+
+    // take all degree one nodes. Note that the above method may follow a path
+    // to a different degree one node, so collect those dead ends and skip them
+    nbrs.toList.filter{ case (_,ns) => ns.size == 1 }
+      .foldLeft( (Set[(Int,Int)](),List[List[(Int,Int)]]()) ){ case ((seen,paths),(node,ns)) =>
+        if (seen.contains(node)) { // skip this node; we have already reached it
+          (seen,paths)
+        } else {
+          // ns has exactly one member
+          val next = ns.head // get that member
+          val (path, bs, endpoint) = follow(node, None, next._1 - node._1, next._2 - node._2, Set())
+          (seen ++ endpoint.toSet, (node :: path) :: paths)
+        }
+      }
+
+    // Now take all degree 3 nodes (T junctions). pick the direction that isn't collinear
   }
 
 
@@ -137,20 +184,40 @@ object MazeDemo {
     type Ctx2D = dom.CanvasRenderingContext2D
     val ctx = c.getContext("2d").asInstanceOf[Ctx2D]
 
-    ctx.strokeStyle = "#7dff9a"
-    ctx.lineWidth = 3
+    ctx.strokeStyle = "#777"
+    ctx.lineWidth = 20
     ctx.lineCap = "round"
     ctx.globalCompositeOperation = "lighter"
 
-    ctx.beginPath()
-    val (w, h) = (50, 50)
+    val (w, h) = (20, 20)
     val k = c.height/(h+2.0)
     val xpos = (c.width - k*w)/2.0
-    mazeLines(w, h, maze(w, h)._1).foreach { line =>
+
+    val walls = maze(w, h)._1
+
+    ctx.beginPath()
+    mazeLines(w, h, walls).foreach { line =>
       ctx.moveTo(xpos + k*line.x0, k*(1+line.y0))
       ctx.lineTo(xpos + k*line.x1, k*(1+line.y1))
     }
     ctx.stroke()
+
+    //ctx.strokeStyle = "#7dff9a"
+    ctx.strokeStyle = "#444"
+    ctx.lineWidth = 4
+
+    val (_, paths) = linkEdges(graph(walls))
+    paths.foreach{ path =>
+      val (x,y) = (xpos + k*path.head._1, k*(1+path.head._2))
+      ctx.beginPath()
+      ctx.moveTo(x-5,y-5)
+      ctx.lineTo(x+5,y+5)
+      ctx.moveTo(x-5,y+5)
+      ctx.lineTo(x+5,y-5)
+      ctx.moveTo(x,y)
+      path.tail.foreach{ case (i,j) => ctx.lineTo(xpos + k*i, k*(1+j)) }
+      ctx.stroke()
+    }
 
   }
 }
